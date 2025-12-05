d571134333340ad604d7dac7789693adc4923721
beenaaddala <beenaaddala@gmail.com>
2025-12-05 18:26:37 +0530

    Add files via upload

---
+++ dailyactivities.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    printf("------ My Daily Activities ------\n");
+    printf("1. Wake up early in the morning\n");
+    printf("2. Brush teeth and fresh up\n");
+    printf("3. Do some exercise / walking\n");
+    printf("4. Take bath and eat breakfast\n");
+    printf("5. Attend classes / study\n");
+    printf("6. Have lunch\n");
+    printf("7. Take rest for some time\n");
+    printf("8. Continue studying / assignments\n");
+    printf("9. Spend time with family / hobbies\n");
+    printf("10. Have dinner\n");
+    printf("11. Relax and plan for next day\n");
+    printf("12. Go to sleep\n");
+    printf("---------------------------------\n");
+
+    return 0;
+}
