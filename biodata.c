99f4cea15befdb6b3224be27307780780fd8d07a
beenaaddala <beenaaddala@gmail.com>
2025-12-05 18:18:21 +0530

    Add files via upload

---
+++ biodata.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("----- BIODATA -----\n");
+    printf("Name        : Beena Addala\n");
+    printf("Age         : 17\n");
+    printf("Gender      : Female\n");
+    printf("Class       : Intermediate / College Student\n");
+    printf("Address     : Andhra Pradesh, India\n");
+    printf("Hobbies     : Reading, Coding, Music\n");
+    printf("--------------------\n");
+
+    return 0;
+}
