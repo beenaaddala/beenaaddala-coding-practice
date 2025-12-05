078cbd60ef0ffa5ea61e4872abf236c4f259947c
beenaaddala <beenaaddala@gmail.com>
2025-12-05 17:27:49 +0530

    Add files via upload

---
+++ prime number or not.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int num,factor =0,i;
+	printf("ente value of num:");
+	scanf("%d",&num);
+	for(i=1;i<=num;i++)
+	{
+		if(num%i==0)
+		{
+			factor=factor+1;
+		}
+	}
+	if(factor>2)
+	{
+		printf("prime number");
+	}
+	else{
+		printf("not a prime number");
+	}
+	return 0;
+}
