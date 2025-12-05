b49854bf7410f2191dc25a8d6667846aaf1ae792
beenaaddala <beenaaddala@gmail.com>
2025-12-05 17:36:06 +0530

    Add files via upload

---
+++ sum of two numbers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	int a,b,Sum;
+	printf("Enter the values of a,b: ");
+	scanf("%d %d" , &a, &b);
+	Sum = a+b;
+	printf("The sum of a and b is: %d\n", Sum);
+	return 0;
+}
